391b96dbff3721d72db889155e4bac758d4819f4
brolaa <bartosz.rola@gmail.com>
2023-11-12 22:32:09 +0100

    Initial Commit

---
+++ src/test/java/com/example/ProjektJava/ProjektJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjektJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
