0158fc9b6c6bc6683eb26ebdd4469ab8c400a0d0
brolaa <bartosz.rola@gmail.com>
2023-11-29 09:11:53 +0100

    fix/Refaktoryzacja kodu i ostatnie szlify

--- src/main/java/com/example/ProjektJava/service/PostMapper.java
+++ src/main/java/com/example/ProjektJava/service/PostMapper.java
@@ -5,27 +5,16 @@ import com.example.ProjektJava.payload.request.PostRequest;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
+// Single responsibility - klasa ma jeden główny cel - zmapować obiekt Post na PostRequest
 @Component
 public class PostMapper {
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("uuuu-MM-dd");
-
     public Post toPost(PostRequest postRequest) {
         return new Post(postRequest.getTitle(),
                 postRequest.getDescription(),
                 postRequest.getContent(),
                 postRequest.getCountry(),
-                LocalDate.parse(postRequest.getDepartDate(), DATE_FORMATTER),
-                LocalDate.parse(postRequest.getReturnDate(), DATE_FORMATTER));
-    }
-
-    public PostRequest toPostRequest(Post post) {
-        return new PostRequest(post.getTitle(),
-                post.getDescription(),
-                post.getContent(),
-                post.getCountry(),
-                post.getDepartDate().format(DATE_FORMATTER),
-                post.getReturnDate().format(DATE_FORMATTER));
+                LocalDate.parse(postRequest.getDepartDate()),
+                LocalDate.parse(postRequest.getReturnDate()));
     }
 }
